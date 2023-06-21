74767b01415095555304a7fbe934a451ff25f495
Henri Wahl <henri.wahl@ukdd.de>
2023-06-21 16:11:51 +0200

    added option to allow disabled tls verifiy

--- backend/connection.py
+++ backend/connection.py
@@ -5,3 +5,7 @@ from backend.config import config
 # gitlab_session to be used to connect to GitLab
 gitlab_session = Session()
 gitlab_session.headers.update({'PRIVATE-TOKEN': config.api.token})
+
+# disable TLS verification if configured
+if config.api.get('verify') == False:
+    gitlab_session.verify = False
