4f43ae4dd3ae84efb36739937c4a5903b3e051cf
HenriWahl <henri@nagstamon.de>
2025-07-29 17:00:55 +0200

    add log on ssl

--- backend/connection.py
+++ backend/connection.py
@@ -2,6 +2,7 @@ from httpx import Client, Response
 
 from backend.config import config, \
     TIMEOUT
+from backend.helpers import log
 
 # disable TLS verification if configured
 if config.api.get('verify', True) == False:
@@ -23,4 +24,5 @@ def gitlab_session_get(url, params=None) -> Response:
         return gitlab_session.get(url, params=params, timeout=TIMEOUT)
     except Exception as exception:
         # if an exception occurs, return a response with status code 999 to indicate an unknown error
+        log.error(f'Exception during access to GitLab: {exception}')
         return Response(999, text=str(exception))
